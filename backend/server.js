ec94ac5cb0fa4f0a84878797bcc4720ab152b7d4
AnuragM23 <anuragmahato006@gmail.com>
2025-04-28 16:05:03 +0530

    created product routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes); // authentication
+app.use("/api/product", productRoutes);
+
 
 app.listen(PORT, ()=>{
     console.log(`server is running at http://localhost:${PORT}`);
