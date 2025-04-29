d8750fb8fd3cf5c80bf4ebfa69bd95271dfa3ee4
AnuragM23 <anuragmahato006@gmail.com>
2025-04-29 13:24:05 +0530

    created payment routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -17,10 +18,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes); // authentication
+app.use("/api/auth", authRoutes); 
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`server is running at http://localhost:${PORT}`);
